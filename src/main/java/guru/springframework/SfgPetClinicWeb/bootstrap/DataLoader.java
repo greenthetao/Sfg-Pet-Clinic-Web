4ab0c738295d1fb660d21c6a8465fd6b947ee860
Harry S. Green <harrysgreen@Harrys-MBP.fios-router.home>
2022-09-28 19:08:13 -0400

    Add new project files to Git

---
+++ src/main/java/guru/springframework/SfgPetClinicWeb/bootstrap/DataLoader.java
@@ -0,0 +1,13 @@
+package guru.springframework.SfgPetClinicWeb.bootstrap;
+
+import src.main.java.guru.springframework.sfgpetclinic.model.*;
+import src.main.java.guru.springframework.sfgpetclinic.services.*;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+public class DataLoader
+{
+
+}
